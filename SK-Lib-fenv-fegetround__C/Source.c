b5f6f0bd858e6c7be347ca267c455744714cbf2e
Rizky Khapidsyah <rizkykhapidsyah@gmail.com>
2021-04-25 11:10:05 +0700

    Add project files.

---
+++ SK-Lib-fenv-fegetround__C/Source.c
@@ -0,0 +1,40 @@
+/* fegetround / rint example */
+#include <stdio.h>      /* printf */
+#include <fenv.h>       /* fegetround, FE_* */
+#include <math.h>       /* rint */
+#include <conio.h>
+
+/*  Source by CppBuzz
+*   Modified by Rizky Khapidsyah
+*/
+
+int main() {
+    printf("rounding using ");
+
+    switch (fegetround()) {
+    case FE_DOWNWARD: 
+        printf("downward"); 
+        break;
+    case FE_TONEAREST: 
+        printf("to-nearest"); 
+        break;
+    case FE_TOWARDZERO: 
+        printf("toward-zero"); 
+        break;
+    case FE_UPWARD: 
+        printf("upward"); 
+        break;
+    default: 
+        printf("unknown");
+    }
+
+    printf(" rounding:\n");
+
+    printf("rint (2.3) = %.1f\n", rint(2.3));
+    printf("rint (3.8) = %.1f\n", rint(3.8));
+    printf("rint (-2.3) = %.1f\n", rint(-2.3));
+    printf("rint (-3.8) = %.1f\n", rint(-3.8));
+ 
+    _getch();
+    return 0;
+}
